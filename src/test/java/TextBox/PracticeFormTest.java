a124585d7bcf5e7b0c615fc77e14d42b026c0230
Sergei Danilov-Skobelev <veresk81@yandex.ru>
2025-12-16 20:55:07 +0400

    Corrections to homework

--- src/test/java/TextBox/PracticeFormTest.java
+++ src/test/java/TextBox/PracticeFormTest.java
@@ -1,8 +1,6 @@
 package TextBox;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.commands.ShouldHave;
-import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -19,7 +17,7 @@ public class PracticeFormTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen =  true;
+        Configuration.holdBrowserOpen =  false;
 
     }
 
@@ -32,13 +30,12 @@ public class PracticeFormTest {
         $("#firstName").setValue("Sergei");
         $("#lastName").setValue("Sergeev");
         $("#userEmail").setValue("sergei_sergeev@mail.ru");
-        $(byText("Male")).click(); //Клик по названию чек-бокса
-        $(byText("Female")).click(); //проверка переключения радоикнопки
-        $(byText("Other")).click();
-        $(byText("Male")).click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("88003002000");
-        $(".react-datepicker__input-container").click(); //Клик по виджету
-        $("[aria-label='Choose Wednesday, December 24th, 2025']").click(); //Выбор значения в текущем месяце
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").$(byText("2000")).click();
+        $(".react-datepicker__month-select").$(byText("September")).click();
+        $(".react-datepicker__day--005").click();
         $("#subjectsContainer input").setValue("in"); //Ввод значения в текстовое поле выпадающего списка
         $(byText("Hindi")).click();
         $(byText("Sports")).click();
@@ -56,7 +53,7 @@ public class PracticeFormTest {
         $(".table-responsive").shouldHave(text("sergei_sergeev@mail.ru"));
         $(".table-responsive").shouldHave(text("Male"));
         $(".table-responsive").shouldHave(text("8800300200"));
-        $(".table-responsive").shouldHave(text("24 December,2025")); //Баг - нет проверки на невозможность ввести будущее время
+        $(".table-responsive").shouldHave(text("05 September,2000")); //Баг - нет проверки на невозможность ввести будущее время
         $(".table-responsive").shouldHave(text("Hindi"));
         $(".table-responsive").shouldHave(text("Sports, Reading"));
         $(".table-responsive").shouldHave(text("example_image_hw3.jpg"));
